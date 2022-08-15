4dc22d10942b020623eeb285a7c7c96bcdb53160
Benny Sufrin <bennysuf@gmail.com>
2022-08-15 18:24:22 -0400

    commit tree 8da253016ab771c517f1b380966ff43defbe3dd8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// let names = ["Guadalupe", "Ollie", "Aki"]
+// let gasp = "surprise"
+function writeCards(names, gasp) {
+    let cards = []
+    let myNames = names
+    let myGasp = gasp
+    for (let i = 0; i < myNames.length; i++) {
+      cards.push(`Thank you, ${myNames[i]}, for the wonderful ${myGasp} gift!`)
+} 
+return cards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], surprise)
+
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i);
+        i--
+    }
+}
